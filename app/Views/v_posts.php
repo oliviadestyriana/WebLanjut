001d0488f048d2d3c913d404322a4f050ebd629f
oliviadestyriana <oliviadestyriana>
2021-09-30 20:40:08 +0700

    Model, migration, seeding

---
+++ app/Views/v_posts.php
@@ -0,0 +1,84 @@
+<div class="container mt-3">
+  <div class="text-center">
+  <h1> All Post </h1>
+</div>
+<div class="row justify-content-center">
+  <div class="col-md-6">
+    <form action="">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<div class="row mt-4">
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?badminton" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Badminton</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?karate" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Karate</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?basketball" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Basketball</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?tenis" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Tenis</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?swimming" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Swimming</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 mb-3">
+    <div class="card">
+      <img src="https://source.unsplash.com/1600x900/?volleyball" class="card-img-top" alt="image">
+      <div class="card-body">
+        <a href="" class="text-decoration-none text-dark">
+          <h5 class="card-title">Vollyball</h5>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
